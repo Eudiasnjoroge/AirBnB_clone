5f419a617116c9955537c77fafd80c56956d36e7
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-11-18 11:59:21 +0300

    Import filestorage

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""__init__ magic method initializes the package"""
+
+#!/usr/bin/python3
+"""__init__ magic method for models directory"""
+
+from models.engine.file_storage import FileStorage
